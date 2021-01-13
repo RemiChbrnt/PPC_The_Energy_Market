2b2610931d6d4271697391697ea32bb3f898824b
Violette154 <50946883+Violette154@users.noreply.github.com>
2021-01-13 11:52:31 +0100

    market

--- market.py
+++ market.py
@@ -3,6 +3,7 @@
             il faudrait définir un prix de base (genre celui de la première itération)
             il faudrait que conso soit aussi une différence (comme ça si elle est négative le prix baisse et inversement)"""
 from multiprocessing import Process
+from queue import Queue
 
 def energPriceChang(precedPrice, carbonPrice, purchasingPow, politic, isTechnicalFailure, isVirus, isFreeEnergyDay, isTsunami, conso):
     newPrice = precedPrice * (1 + conso * 0.001)
@@ -19,16 +20,35 @@ def energPriceChang(precedPrice, carbonPrice, purchasingPow, politic, isTechnica
         if isTsunami == true :
             newPrice = newPrice + 0.025
 
+        print(newPrice)
+
+def economics (qEco) :
+    carbonPrice = 0.001
+    purchasingPow = 5;
+    eco = [carbonPrice, purchasingPow]
+    while true:
+        purchasingPow = purchasingPow + random.randint(-1, 1)
+        eco = [carbonPrice, purchasingPow]
+        qEco.put(eco)
+
 
 if __name__ == "__main__":
-    carbonPrice =
-    purchasingPow =
-    politic =
-    isTechnicalFailure =
-    isVirus =
-    isFreeEnergyDay =
-    isTsunami =
-    conso =
+    qEco = Queue()
+    #lancement process economics
+    pEco = Process(target = economics, args = (qEco,))
+    pEco.start()
+    pEco.join()
+
+    resEco = q.get()
+    precedPrice = 0.1
+    carbonPrice = res [0]
+    purchasingPow = res[1]
+    politic = false
+    isTechnicalFailure = false
+    isVirus = false
+    isFreeEnergyDay = false
+    isTsunami = false
+    conso = 10
     p = Process(target = energPriceChang, args = (precedPrice, carbonPrice, purchasingPow, politic, isTechnicalFailure, isVirus, isFreeEnergyDay, isTsunami, conso,))
     p.start()
     p.join()
